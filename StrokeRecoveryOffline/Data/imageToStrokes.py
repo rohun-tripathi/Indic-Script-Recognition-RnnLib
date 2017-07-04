115b52496a6bf86c887c49e4eb47d7b029c82aa4
rohun-tripati <tripati.riot@gmail.com>
2017-07-04 15:45:07 +0530

    moved_old_code

--- StrokeRecoveryOffline/Data/imageToStrokes.py
+++
@@ -1,7 +0,0 @@
-#STROKE RECOVERY
-#This is the start point of our stroke recovery code.
-#This calls the file which recursively enters folders and generates the Strokes for each image file
-
-import code.wordToTxt as MN
-
-MN.recursiveStrokeRecovery()
